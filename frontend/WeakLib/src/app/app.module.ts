b37798088366958661d6c2b53f6b6c82fc228b31
Domenik Irrgang <domenikirrgang@web.de>
2017-07-31 11:03:02 +0200

    Added routing

--- frontend/WeakLib/src/app/app.module.ts
+++ frontend/WeakLib/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BoxComponent } from './box/box.component';
 
+import { RoutingModule } from './routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     BoxComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
