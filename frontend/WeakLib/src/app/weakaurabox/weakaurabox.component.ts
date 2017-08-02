beacb15f94c01e5a896e54a771c90ca3d950bdcb
Domenik Irrgang <domenikirrgang@web.de>
2017-08-02 11:11:37 +0200

    added box with heading

--- frontend/WeakLib/src/app/weakaurabox/weakaurabox.component.ts
+++ frontend/WeakLib/src/app/weakaurabox/weakaurabox.component.ts
@@ -1,12 +1,14 @@
-import {Component,Input, Directive,ElementRef} from '@angular/core';
-import {Weakaura} from '../util/Weakaura';
+import { Component, Input, Directive, ElementRef } from '@angular/core';
+import { Weakaura } from '../util/Weakaura';
 
 @Component({
     selector: 'weakaurabox',
     templateUrl: './weakaurabox.component.html',
     styleUrls: ['./weakaurabox.component.css'],
-    
+
 })
-export class WeakauraBoxComponent{
- @Input() weakauras: Weakaura[] ;
+export class WeakauraBoxComponent {
+    @Input() weakauras: Weakaura[];
+    @Input() heading: String;
+    @Input() searchURL: String;
 }
