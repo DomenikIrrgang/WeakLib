3e51baf66909a0a621316d73a428d09fb3b3a937
Domenik Irrgang <domenikirrgang@web.de>
2017-08-04 11:21:27 +0200

    fixed typo in titlebarcomponent

--- frontend/WeakLib/src/app/weakauraview/titlebar/titlebar.component.ts
+++ frontend/WeakLib/src/app/weakauraview/titlebar/titlebar.component.ts
@@ -1,13 +1,12 @@
-import {Component,Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Weakaura } from '../../util/Weakaura';
 import { WEAKAURAS } from '../../mockdata/weakauras';
 
 @Component({
-selector: 'titlebar',
-templateUrl: './titlebar.component.html',
-styleUrls:['./titlebar.component.css'],
+    selector: 'titlebar',
+    templateUrl: './titlebar.component.html',
+    styleUrls: ['./titlebar.component.css'],
 })
-export class TitlebarComponent{
-@Input() weakura: Weakaura;
-
+export class TitlebarComponent {
+    @Input() weakaura: Weakaura;
 }
