8fb74469615a94317ea3543e0ae249c3b3e33fbf
Domenik Irrgang <domenikirrgang@web.de>
2017-08-06 11:27:20 +0200

    implemented display of comments

--- frontend/WeakLib/src/app/mockdata/comment.ts
+++ frontend/WeakLib/src/app/mockdata/comment.ts
@@ -2,17 +2,17 @@ import { Comment } from '../util/comment';
 
 export const COMMENTS: Comment[] = [
     {
-        author: { name: 'user1', profilePicture: "http://wow.zamimg.com/images/logos/construction.png" },
+        author: { name: 'user1', profilePicture: "http://orig01.deviantart.net/12c5/f/2014/085/f/b/kanra_icon__128x128__by_akashizaya-d7bsehd.png" },
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         timestamp: Date.now(),
     },
     {
-        author: { name: 'user2', profilePicture: "http://wow.zamimg.com/images/logos/construction.png" },
+        author: { name: 'user2', profilePicture: "http://orig01.deviantart.net/12c5/f/2014/085/f/b/kanra_icon__128x128__by_akashizaya-d7bsehd.png" },
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         timestamp: Date.now(),
     },
     {
-        author: { name: 'user3', profilePicture: "http://wow.zamimg.com/images/logos/construction.png" },
+        author: { name: 'user3', profilePicture: "http://orig01.deviantart.net/12c5/f/2014/085/f/b/kanra_icon__128x128__by_akashizaya-d7bsehd.png" },
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         timestamp: Date.now(),
     },
