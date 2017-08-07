765fdecb56eafb595c6ec8027a12d834f1453107
Domenik Irrgang <domenikirrgang@web.de>
2017-08-07 11:58:23 +0200

    changed styling in weakauraview

--- frontend/WeakLib/src/app/clickablepanel/clickablepanel.component.ts
+++ frontend/WeakLib/src/app/clickablepanel/clickablepanel.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class ClickablePanelComponent implements OnInit {
     @Input() heading: String;
-    contentVisible: boolean = false;
+    @Input() contentVisible: boolean;
 
     constructor() { }
 
