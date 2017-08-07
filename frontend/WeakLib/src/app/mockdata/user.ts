d55e31054b8f307d6d9d62944cdc120bfe0c016d
Gamko2 <3077-916@online.de>
2017-08-07 11:16:05 +0200

    user description

--- frontend/WeakLib/src/app/mockdata/user.ts
+++ frontend/WeakLib/src/app/mockdata/user.ts
@@ -2,15 +2,18 @@ import { User } from '../util/user';
 
 export const USER: User[] = [
     {
-        name: 'user1', profilePicture: "http://wow.zamimg.com/images/logos/construction.png"
+        name: 'user1', profilePicture: "http://wow.zamimg.com/images/logos/construction.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-        name: 'user2', profilePicture: "http://wow.zamimg.com/images/logos/construction.png"
+        name: 'user2', profilePicture: "http://wow.zamimg.com/images/logos/construction.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-        name: 'user3', profilePicture: "http://wow.zamimg.com/images/logos/construction.png"
-    },
+        name: 'user3', profilePicture: "http://wow.zamimg.com/images/logos/construction.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    },
     {
-        name: 'user4', profilePicture: "http://wow.zamimg.com/images/logos/construction.png"
+        name: 'user4', profilePicture: "http://wow.zamimg.com/images/logos/construction.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
 ];
