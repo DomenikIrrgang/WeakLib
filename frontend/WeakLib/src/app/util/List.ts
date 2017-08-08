2174433fd2e069fad160d861a61a3c4050dfb713
Domenik Irrgang <domenikirrgang@web.de>
2017-08-08 11:46:16 +0200

    styled categories

--- frontend/WeakLib/src/app/util/List.ts
+++ frontend/WeakLib/src/app/util/List.ts
@@ -17,6 +17,28 @@ export class List<T> {
         return this.items[index];
     }
 
+    contains(value: T): boolean {
+        var i: number;
+        for (i = 0; i < this.size(); i++) {
+            if (this.get(i) == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    remove(value: T): void {
+        var i: number;
+        var assign: number = 0;
+        for (i = 0; i < this.size(); i++) {
+            if (value == this.get(i)) {
+                assign = 1;
+            }
+            this.items[i] = this.items[i + assign];
+        }
+        this.items.length = this.items.length - 1;
+    }
+
     toArray(): Array<T> {
         return this.items;
     }
