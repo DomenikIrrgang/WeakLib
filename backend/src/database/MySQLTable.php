15461e94dc06d97a28b301c70929a3075f55ed9b
Domenik Irrgang <domenikirrgang@web.de>
2017-08-14 10:53:49 +0200

    Added Database

--- backend/src/database/MySQLTable.php
+++ backend/src/database/MySQLTable.php
@@ -6,15 +6,15 @@ abstract class MySQLTable
     protected $fields = array();
     protected $foreignKeys = array();
 
-    public function setName($name) {
+    public function setName(string $name) {
         $this->name = $name;
     }
 
-    public function addField($name, $type) {
+    public function addField(string $name, string $type) {
         array_push($fields, new Field($name, $type));
     }
 
-    public function addForeignKey($name, $type, $reference, $table) {
+    public function addForeignKey(string $name, string $type, string $reference, string $table) {
         array_push($foreignKeys, new ForeignKey(new Field($name, $type), $reference, $table));
     }
 }
