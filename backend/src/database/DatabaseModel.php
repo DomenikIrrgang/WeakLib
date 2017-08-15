e7dd8b3764ecb9e8ca6be752e2b9ef0163879c97
Domenik Irrgang <domenikirrgang@web.de>
2017-08-15 11:00:44 +0200

    added functions to database model

--- backend/src/database/DatabaseModel.php
+++ backend/src/database/DatabaseModel.php
@@ -1,6 +1,9 @@
 <?php
 
+require_once './database/Database.php';
+
 interface DatabaseModel
 {
-
+    public function add(Database $database): bool;
+    public function remove(Database $database): bool;
 }
