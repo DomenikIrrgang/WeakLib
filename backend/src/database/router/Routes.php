276986d582b0a502e5d0be3c654683f9d79c2b36
Domenik Irrgang <domenikirrgang@web.de>
2017-08-19 11:37:05 +0200

    fixed getallusercontroller

--- backend/src/database/router/Routes.php
+++ backend/src/database/router/Routes.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once "./database/router/Router.php";
-require_once "./database/router/controller/ErrorController.php";
-require_once "./database/router/controller/LoginController.php";
+foreach (glob("./database/router/controller/*.php") as $filename)
+{
+    require_once $filename;
+}
 
 $router = new Router();
 $router->error404 = new Route("error", "/error", new ErrorController(404, "Could not find page"));
 $router->get(new Route("test", "/test", new LoginController()));
+$router->get(new Route("GET AllUser", "/api/user", new GetAllUserController()));
--- backend/src/database/router/controller/GetAllUserController.php
+++ backend/src/database/router/controller/GetAllUserController.php
@@ -2,14 +2,14 @@
 require_once "./database/router/controller/Controller.php";
 require_once "./database/tables/UserTable.php";
 
-class GetAllUserController implements Controller {
-    public function request(array $params): string{
+class GetAllUserController implements Controller
+{
+    public function request(array $params): string
+    {
+        global $database;
         $userTable = new UserTable;
-        $allData=$userTable.getAllData($database);
+        $allData = $userTable->getAllData($database);
         var_dump($allData);
         return "";
-
     }
-
-
 }
