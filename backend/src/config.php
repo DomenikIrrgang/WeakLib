e8e2cc98cf5c905e5c337afde2cebe0a87effe12
Gamko2 <3077-916@online.de>
2017-09-08 18:59:51 +0200

    uploadwa

--- backend/src/config.php
+++ backend/src/config.php
@@ -25,4 +25,4 @@ $config["database"]["ip"] = "127.0.0.1";
 $config ["database"]["name"]="weaklib";
 $config ["database"]["username"]="root";
 $config ["database"]["password"] ="root";
-$config ["database"]["port"]= 3306;
+$config ["database"]["port"]= 8080;
