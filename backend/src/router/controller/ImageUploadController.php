6a5b3df3ef332e163e526289ace590689f28c089
Domenik Irrgang <domenikirrgang@web.de>
2017-09-11 13:47:55 +0200

    added typecheck and support for multiple files

--- backend/src/router/controller/ImageUploadController.php
+++ backend/src/router/controller/ImageUploadController.php
@@ -4,28 +4,56 @@ require_once "./database/tables/WeakauraTable.php";
 
 class ImageUploadController implements Controller
 {
+    private $allowed =  array('gif','png' ,'jpg');
+
     public function request(Request $request, array $params): string
     {
-        global $database;
         global $config;
 
-        if ($_FILES["file"]["error"] == UPLOAD_ERR_NO_FILE) {
-                return "ERROR nofile";
+        //var_dump($_FILES["file"]);
+        $uploads = [];
+        for ($i = 0; $i < count($_FILES["file"]["name"]); $i++) {
+            array_push($uploads, $this->uploadFile($_FILES["file"], $i));
+        }
+        return json_encode($uploads);
+    }
+    
+    private function uploadFile(array $files, int $index)
+    {
+        global $config;
+        $result = [];
+        $result["name"] = $files["name"][$index];
+        $result["error"] = "OK";
+        $result["url"] = "";
+
+        if ($files["error"][$index] == UPLOAD_ERR_NO_FILE) {
+            $result["error"] = "no file";
+            return $result;
         }
 
-        if ($_FILES["file"]["size"] > $config["imageupload"]["maxSize"]) {
-            return "ERROR size";
+        if ($files["size"][$index] > $config["imageupload"]["maxSize"]) {
+            $result["error"] = "size";
+            return $result;
         }
 
-        if ($_FILES["file"]["error"] > 0) {
-            return "ERROR unknown";
+        if ($files["error"][$index] > 0) {
+            $result["error"] = "unknown";
+            return $result;
         }
 
         $name = hash('sha256', rand());
-        $extension = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
-        if (!move_uploaded_file($_FILES['file']['tmp_name'], $config["imageupload"]["directory"] . $name . "." . $extension)) {
-            return "ERROR internal";
+        $extension = pathinfo($files["name"][$index], PATHINFO_EXTENSION);
+
+        if(!in_array($extension, $this->allowed) ) {
+            $result["error"] = "invalid type";
+            return $result;
         }
-        return $config["imageupload"]["directory"] . $name . "." . $extension;
+
+        if (!move_uploaded_file($files['tmp_name'][$index], $config["imageupload"]["directory"] . $name . "." . $extension)) {
+            $result["error"] = "internal";
+            return $result;
+        }
+        $result["url"] = $config["imageupload"]["directory"] . $name . "." . $extension;
+        return $result;
     }
 }
